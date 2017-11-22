fc024c1c0d47ac04849cd1550276314bf8ba3d6f
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-11-22 11:08:58 +0100

    ajout modeles restaurant et review

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+// app/assets/javascripts/application.js
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
