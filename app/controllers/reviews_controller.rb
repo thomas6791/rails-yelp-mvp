6d6f405d13f32eb78b1eb83354921eb56c5785e6
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-11-22 11:17:02 +0100

    ajout des controllers restaurants et reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
+  get 'reviews/edit'
+
+  get 'reviews/new'
+
+  get 'reviews/show'
+
+  get 'reviews/update'
+
+  get 'restaurants/index'
+
+  get 'restaurants/create'
+
+  get 'restaurants/destroy'
+
+  get 'restaurants/edit'
+
+  get 'restaurants/new'
+
+  get 'restaurants/show'
+
+  get 'restaurants/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
