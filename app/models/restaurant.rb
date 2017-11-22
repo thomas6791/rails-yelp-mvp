5e2b4cfd1b69eaec5135e1c228a86739821c8c0d
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-11-22 17:47:34 +0100

    ajout yelp final

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :phone_number, presence: true
   validates :name, presence: true
   validates :address, presence: true
